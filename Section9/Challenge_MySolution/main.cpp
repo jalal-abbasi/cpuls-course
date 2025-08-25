4ce63ac7bcd01d5c54014ea7e6386f2640ae7535
jalal-abbasi <s.jalalabasi94@gmail.com>
2025-08-25 11:19:31 +0200

    Completing case 'P'

--- Section9/Challenge_MySolution/main.cpp
+++ Section9/Challenge_MySolution/main.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <typeinfo>
+#include <limits>
+#include <ios>
+
 using namespace std;
 
 int main(){
     
-//    vector<int> numbers{2,3,4,8,5,9};
-    vector<int> numbers;
+    vector<int> numbers{3,4,8,5,9,2};
+//    vector<int> numbers;
+    vector<char> options{'P','A', 'M', 'S', 'L', 'Q'};
+    int is_not_integer{0};
+    
 
-    cout << "P - Print numbers" "\n"
-    "A - Add a number" "\n" 
-    "M - Display mean of the numbers" "\n" 
-    "S - Display the smallest number" "\n" 
-    "L - Display the largest number" "\n" 
-    "Q - Quit" << endl;
-    
-    cout << "\n" "Enter your choice: ";
-    
     char choice;
     
-    cin >> choice;
-    choice = toupper(choice);
-    
-    
-    switch (choice){
+    do{
+      
+        cout << "P - Print numbers" "\n"
+        "A - Add a number" "\n" 
+        "M - Display mean of the numbers" "\n" 
+        "S - Display the smallest number" "\n" 
+        "L - Display the largest number" "\n" 
+        "Q - Quit" << endl;
+        
+        cout << "\n" "Enter your choice: ";
+        cin >> choice;
+//        cout << "\n" << choice << "\n" << endl;
+        cin.sync();
+        choice = toupper(choice);
+        
+        if (find(options.begin(),options.end(), choice)==options.end())
+            cout << "Unknown selection, please try again" << endl;
+            
+        switch (choice){
         case 'P':{
             if (numbers.size() !=0){
                 cout << "[ ";
@@ -36,17 +49,30 @@ int main(){
             break;
         }
         case 'A':
-            cout << "\n""Bye" ;
+            
             break;
+            
         case 'M':
-            cout << "\n""Bye2" ;
+            
             break;
+            
         case 'S':
+            
             break;
         case 'L':
+            
             break;
         case 'Q':
+            
             break;
+
+        }
     }
+while(find(options.begin(),options.end(), choice)==options.end() || is_not_integer);
+    
+    
+    
+    
+    
     return 0;
 }
