bf9d54acdafc263f577c025085e77b34628604ee
jalal-abbasi <s.jalalabasi94@gmail.com>
2025-08-25 15:54:58 +0200

    Finishing the first coding exercise of the section

--- Section10/test/main.cpp
+++ Section10/test/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cstring>
+#include <cctype>
 
 using namespace std;
 int main() {
-    cout << "Hello World!" << endl;
-    return 0;
+    char first_name[20]{"Bjarne"};
+    char last_name[20]{"Stroustrup"};
+    char whole_name[50]{};
+    
+    
+    size_t first_name_length{strlen(first_name)};
+    size_t last_name_length{strlen(last_name)};
+    
+    strcpy(whole_name,first_name);
+    strcat(whole_name, last_name);
+    
+    size_t whole_name_length(strlen(whole_name));
+    
 }
