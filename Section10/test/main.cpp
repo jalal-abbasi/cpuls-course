f815912cd3155fa8f6121fa5d89cc957e6bf042c
jalal-abbasi <s.jalalabasi94@gmail.com>
2025-08-25 16:14:11 +0200

    Completing the coding exercise 2 of the section (Coding exercise 21)

--- Section10/test/main.cpp
+++ Section10/test/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
-#include <cstring>
-#include <cctype>
+//#include <cstring>
+//#include <cctype>
+#include <string>
 
 using namespace std;
 int main() {
-    char first_name[20]{"Bjarne"};
-    char last_name[20]{"Stroustrup"};
-    char whole_name[50]{};
+    
+//    ################# first section exercise ############
+//    char first_name[20]{"Bjarne"};
+//    char last_name[20]{"Stroustrup"};
+//    char whole_name[50]{};
+//    
+//    
+//    size_t first_name_length{strlen(first_name)};
+//    size_t last_name_length{strlen(last_name)};
+//    
+//    strcpy(whole_name,first_name);
+//    strcat(whole_name, last_name);
+//    
+//    size_t whole_name_length(strlen(whole_name));
+
+   
+//    ################ second section exercise ###########
+    string unformatted_full_name {"StephenHawking"};
+    string first_name{unformatted_full_name,0,7};
+    string last_name{};
+    
+    last_name = unformatted_full_name.substr(7,7);
+    
+    string formatted_full_name{};
+    formatted_full_name = first_name + last_name;
     
     
-    size_t first_name_length{strlen(first_name)};
-    size_t last_name_length{strlen(last_name)};
-    
-    strcpy(whole_name,first_name);
-    strcat(whole_name, last_name);
-    
-    size_t whole_name_length(strlen(whole_name));
-    
+    formatted_full_name.insert(7," ");
+    cout << formatted_full_name;
 }
