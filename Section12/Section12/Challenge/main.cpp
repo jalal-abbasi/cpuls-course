290e753dd7ed107dfdfa02981bd7a6245bd41558
jalal-abbasi <s.jalalabasi94@gmail.com>
2026-04-22 14:55:45 +0200

    finishing chapters 11 and 12

---
+++ Section12/Section12/Challenge/main.cpp
@@ -0,0 +1,91 @@
+// Section 12
+// Challenge
+
+/* Write a C++ function named apply_all that expects two arrays of integers and their sizes and
+  dynamically allocates a new array of integers whose size is the product of the 2 array sizes
+  
+  The function should loop through the second array and multiplies each element across each element of array 1 and store the
+  product in the newly created array.
+  
+  The function should return a pointer to the newly allocated array.
+  
+  You can also write a print function that expects a pointer to an array of integers and its size and display the
+  elements in the array.
+    
+  For example,
+  
+  Below is the output from the following code which would be in main:
+  
+    int array1[] {1,2,3,4,5};
+    int array2[] {10,20,30};
+    
+    cout << "Array 1: " ;
+    print(array1,5);
+    
+    cout << "Array 2: " ;
+    print(array2,3);
+    
+    int *results = apply_all(array1, 5, array2, 3);
+    cout << "Result: " ;
+    print(results,15);
+   
+   Output
+   ---------------------
+   Array 1: [ 1 2 3 4 5 ]
+   Array 2: [ 10 20 30 ]
+   Result: [ 10 20 30 40 50 20 40 60 80 100 30 60 90 120 150 ]
+*/
+  
+#include <iostream>
+
+using namespace std;
+void print(const int [], size_t);
+int *const apply_all(const int [], size_t, const int [], size_t);
+
+
+void print(const int arr[], size_t size_arr){
+  cout << "[ ";
+  for (size_t i{0}; i < size_arr ; i++){
+    cout << arr[i] << " ";
+  }
+    
+  cout << "]" << endl;
+}
+
+int *const apply_all(const int array_1[], size_t size_1, const int array_2[], size_t size_2){
+  int *arr_ptr = new int[size_1*size_2];
+  int k {0};
+  for (size_t i=0; i < size_1; i++){
+    for (size_t j=0; j<size_2; j++){
+      *(arr_ptr + k) = array_1[i]*array_2[j];
+      k++;
+    }
+  }
+  return arr_ptr;
+}
+
+
+int main() {
+    const size_t array1_size {5};
+    const size_t array2_size {3};
+    
+    int array1[] {1,2,3,4,5};
+    int array2[] {10,20,30};
+    
+    cout << "Array 1: " ;
+    print(array1,array1_size);
+    
+    cout << "Array 2: " ;
+    print(array2,array2_size);
+    
+    int *results = apply_all(array1, array1_size, array2, array2_size);
+    constexpr size_t results_size {array1_size * array2_size};
+
+    cout << "Result: " ;
+    print(results, results_size);
+    
+    cout << endl;
+    delete [] results;
+    return 0;
+}
+
